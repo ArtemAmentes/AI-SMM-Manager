5f16700b33ef8d1f6ce558895baea41a4b6de4c8
Artem Amentes <artem@socialcode.ru>
2022-11-15 22:18:59 +0300

    Добавлено приложение на фласк для создания web приложения (пока шаблон в форме блога и обновлено

--- backend/app.py
+++ backend/app.py
@@ -1,85 +1,58 @@
-import sqlite3
-from flask import Flask, render_template, request, url_for, flash, redirect
-from werkzeug.exceptions import abort
+import os
+import psycopg2
+from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'your secret key'
-
 def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
+    conn = psycopg2.connect(user="cvcode",
+                                  # пароль, который указали при установке PostgreSQL
+                                  password="cvcode",
+                                  host="45.8.248.84",
+                                  port="5432",
+                                  database="cvcode_database")
     return conn
 
-def get_post(post_id):
-    conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
-    conn.close()
-    if post is None:
-        abort(404)
-    return post
 
 @app.route('/')
 def index():
     conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM books;')
+    books = cur.fetchall()
+    cur.execute('SELECT * FROM slaves;')
+    slaves = cur.fetchall()
+    cur.close()
     conn.close()
-    return render_template('index.html', posts=posts)
+    return render_template('index.html', books=books, slaves=slaves)
 
-
-@app.route('/<int:post_id>')
-def post(post_id):
-    post = get_post(post_id)
-    return render_template('post.html', post=post)
-
-@app.route('/create', methods=('GET', 'POST'))
+@app.route('/create/', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
         title = request.form['title']
-        content = request.form['content']
+        author = request.form['author']
+        pages_num = int(request.form['pages_num'])
+        review = request.form['review']
 
-        if not title:
-            flash('Title is required!')
-        else:
-            conn = get_db_connection()
-            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('index'))
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO books (title, author, pages_num, review)'
+                    'VALUES (%s, %s, %s, %s)',
+                    (title, author, pages_num, review))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect(url_for('index'))
 
     return render_template('create.html')
-@app.route('/<int:id>/edit', methods=('GET', 'POST'))
-def edit(id):
-    post = get_post(id)
 
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
+@app.route('/vacancy')
+def vacancy():
 
-        if not title:
-            flash('Title is required!')
-        else:
-            conn = get_db_connection()
-            conn.execute('UPDATE posts SET title = ?, content = ?'
-                         ' WHERE id = ?',
-                         (title, content, id))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('index'))
+    return render_template('vacancy.html')
 
-    return render_template('edit.html', post=post)
 
-@app.route('/<int:id>/delete', methods=('POST',))
-def delete(id):
-    post = get_post(id)
-    conn = get_db_connection()
-    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
-    conn.commit()
-    conn.close()
-    flash('"{}" was successfully deleted!'.format(post['title']))
-    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
--- backend/init_db.py
+++ backend/init_db.py
@@ -1,20 +1,46 @@
-import sqlite3
+import os
+import psycopg2
 
-connection = sqlite3.connect('database.db')
+conn = psycopg2.connect(user="cvcode",
+                                  # пароль, который указали при установке PostgreSQL
+                                  password="cvcode",
+                                  host="45.8.248.84",
+                                  port="5432",
+                                  database="cvcode_database")
 
+# Open a cursor to perform database operations
+cur = conn.cursor()
 
-with open('schema.sql') as f:
-    connection.executescript(f.read())
+# Execute a command: this creates a new table
+cur.execute('DROP TABLE IF EXISTS books;')
+cur.execute('CREATE TABLE books (id serial PRIMARY KEY,'
+                                 'title varchar (150) NOT NULL,'
+                                 'author varchar (50) NOT NULL,'
+                                 'pages_num integer NOT NULL,'
+                                 'review text,'
+                                 'date_added date DEFAULT CURRENT_TIMESTAMP);'
+                                 )
 
-cur = connection.cursor()
+# Insert data into the table
 
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('First Post', 'Content for the first post')
+cur.execute('INSERT INTO books (title, author, pages_num, review)'
+            'VALUES (%s, %s, %s, %s)',
+            ('A Tale of Two Cities',
+             'Charles Dickens',
+             489,
+             'A great classic!')
             )
 
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('Second Post', 'Content for the second post')
+
+cur.execute('INSERT INTO books (title, author, pages_num, review)'
+            'VALUES (%s, %s, %s, %s)',
+            ('Anna Karenina',
+             'Leo Tolstoy',
+             864,
+             'Another great classic!')
             )
 
-connection.commit()
-connection.close()
+conn.commit()
+
+cur.close()
+conn.close()
